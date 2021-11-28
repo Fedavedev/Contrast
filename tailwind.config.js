6c782ac9de3e72d2e642c48b129a5ce0d59690b2
Adewale David <adewaledavedev@gmail.com>
2021-11-28 18:53:42 +0100

    A contrast landing page challenge

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: "#4D13D1",
+        black: "#1C2321",
+        gray: "#595959",
+        search: "#F9FAFB",
+        card: "#E5E7EB",
+        overlay: "#cec3f9",
+      },
+    },
+
+    height: {
+      sm: "500px",
+    },
+
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1300px",
+    },
+
+    fontFamily: {
+      archivo: ["Archivo", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
