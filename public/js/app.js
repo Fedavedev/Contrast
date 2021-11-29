8c11ccdfd95a73ec53963407e2f31480284f95b5
Adewale David <adewaledavedev@gmail.com>
2021-11-29 10:14:36 +0100

    Added CSS animated hamburger

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,10 @@ $("document").ready(function () {
   $(".hamburger").click(function () {
     $(".mobile-menu").toggleClass("open-menu");
   });
+
+    $(".hamburger-btn").click(function () {
+        $(".line-2").toggleClass("close");
+        $(".line-1").toggleClass("open-1");
+        $(".line-3").toggleClass("open-2");
+      });
 });
